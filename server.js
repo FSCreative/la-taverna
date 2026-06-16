f46e00e9e7111b7591598fd85fbcdccd30936037
FSCreative <simon@fs-creative.at>
2026-06-16 12:03:36 +0200

    SEO: Meta, OG, JSON-LD, robots.txt, sitemap.xml

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ const MIME = {
   '.js': 'application/javascript; charset=utf-8', '.json': 'application/json; charset=utf-8',
   '.png': 'image/png', '.jpg': 'image/jpeg', '.jpeg': 'image/jpeg', '.gif': 'image/gif',
   '.svg': 'image/svg+xml', '.webp': 'image/webp', '.ico': 'image/x-icon',
-  '.woff': 'font/woff', '.woff2': 'font/woff2', '.txt': 'text/plain; charset=utf-8'
+  '.woff': 'font/woff', '.woff2': 'font/woff2', '.txt': 'text/plain; charset=utf-8',
+  '.xml': 'application/xml; charset=utf-8', '.webmanifest': 'application/manifest+json'
 };
 const EXT_FOR_MIME = {
   'image/png': '.png', 'image/jpeg': '.jpg', 'image/jpg': '.jpg',
